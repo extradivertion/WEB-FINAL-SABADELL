754c1079a0bd0c7895b3c94307d3cab029aa3cce
extradivertion <smextradivertion@gmail.com>
2026-05-15 15:26:52 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -9,5 +9,7 @@ const currentPath = window.location.pathname.replace(/\/$/, "") || "/";
 const localSeoPage = localSeoPages.find((page) => page.path === currentPath);
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>{localSeoPage ? <LocalSeoPageView page={localSeoPage} /> : <App />}</StrictMode>
+  <StrictMode>
+    {localSeoPage ? <LocalSeoPageView page={localSeoPage} /> : <App />}
+  </StrictMode>
 );
